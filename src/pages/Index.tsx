81ad789e1519334c9470fb76d65f794d9ed94cd0
[dyad] <cstudioao@gmail.com>
2025-11-01 12:28:42 +0000

    [dyad] Substituído o carrossel da página inicial por um slider moderno - wrote 5 file(s), deleted 1 file(s), added framer-motion package(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import HeroCarousel from "@/components/HeroCarousel";
+import HeroSlider from "@/components/HeroSlider";
 import FeaturesSection from "@/components/FeaturesSection";
 import TransformationShowcase from "@/components/TransformationShowcase";
 import ProcessSection from "@/components/ProcessSection";
@@ -20,7 +20,7 @@ const Index = () => {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <HeroCarousel />
+      <HeroSlider />
       <StatsSection />
       <TransformationShowcase />
       <FeaturesSection />
