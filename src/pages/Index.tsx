e749f663e4359c66a675e2404c24df13df64d3d7
[dyad] <cstudioao@gmail.com>
2025-11-01 12:19:27 +0000

    [dyad] Added a footer to the landing page and improved the user experience with animations and interactive effects. - wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,6 +14,7 @@ import CTASection from "@/components/CTASection";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import PricingSection from "@/components/PricingSection";
 import FinalCTA from "@/components/FinalCTA";
+import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
@@ -34,6 +35,7 @@ const Index = () => {
       <TestimonialsSection />
       <PricingSection />
       <FinalCTA />
+      <Footer />
     </div>
   );
 };
